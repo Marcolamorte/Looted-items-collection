e881d9e308da8db10a928ba3be0ad6589a568148
Corrado Consiglio <corrado.consiglio@studio.unibo.it>
2025-02-04 12:25:19 +0100

    various changes

--- collection.js
+++ collection.js
@@ -1,23 +1,23 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.querySelectorAll(".grid-item").forEach(item => {
-        item.addEventListener("click", function(event) {
-            event.preventDefault(); 
-            const selectedItem = this.getAttribute("data-item");
+// document.addEventListener("DOMContentLoaded", function() {
+//     document.querySelectorAll(".grid-item").forEach(item => {
+//         item.addEventListener("click", function(event) {
+//             event.preventDefault(); 
+//             const selectedItem = this.getAttribute("data-item");
             
             
-            localStorage.setItem("selectedTour", "Timeline Tour");
-            localStorage.setItem("selectedItem", selectedItem);
+//             localStorage.setItem("selectedTour", "Timeline Tour");
+//             localStorage.setItem("selectedItem", selectedItem);
 
             
-            console.log("Oggetto selezionato:", selectedItem);
-            console.log("Tour selezionato:", localStorage.getItem("selectedTour"));
-            console.log("Oggetto salvato in localStorage:", localStorage.getItem("selectedItem"));
+//             console.log("Oggetto selezionato:", selectedItem);
+//             console.log("Tour selezionato:", localStorage.getItem("selectedTour"));
+//             console.log("Oggetto salvato in localStorage:", localStorage.getItem("selectedItem"));
 
-           window.location.href = "map_page.html";
+//            window.location.href = "map_page.html";
 
-        });
-    });
-});
+//         });
+//     });
+// });
 
 
 
